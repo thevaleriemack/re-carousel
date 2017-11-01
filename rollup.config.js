c1538ca2b52664e175ca80692d2f6abb5b8152a9
Amio <amio.cn@gmail.com>
2017-11-01 10:45:37 +0800

    Update rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
-  entry: 'src/carousel.js',
-  dest: 'dist/carousel.js',
-  format: 'cjs',
+  input: 'src/carousel.js',
+  output: {
+    file: 'dist/carousel.js',
+    format: 'cjs'
+  },
   plugins: [ buble(), uglify() ],
   external: ['react', 'react-dom', 'prop-types']
 }
