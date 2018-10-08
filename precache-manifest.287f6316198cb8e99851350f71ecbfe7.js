eac3dbd43afda2aa90fb27e766d40e1f69ec9fb9
Amio <amio.cn@gmail.com>
2018-10-08 14:24:58 +0800

    Updates

---
+++ precache-manifest.287f6316198cb8e99851350f71ecbfe7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5281133f55c39860509",
+    "url": "/re-carousel/static/js/runtime~main.d5281133.js"
+  },
+  {
+    "revision": "9b6abb91f10368f2bff7",
+    "url": "/re-carousel/static/js/main.9b6abb91.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/re-carousel/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "07cb4bcbeb8d0e66f43845e1e6dfc608",
+    "url": "/re-carousel/index.html"
+  }
+];
